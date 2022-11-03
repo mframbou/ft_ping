aba6edf747cd1fa4642bb729bc9ba179c050c3c2
Mederis Frambourt <mframbou@student.42nice.fr>
2022-11-03 21:25:36 +0100

    Libft

---
+++ srcs/libft/strings/ft_rm_char.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                       .-.                       .                          */
+/*                      / -'                      /                           */
+/*       .  .-. .-.   -/--).--..-.  .  .-. .-.   /-.  .-._.)  (               */
+/*        )/   )   )  /  /    (  |   )/   )   ) /   )(   )(    )         .    */
+/*   By: '/   /   (`.'  /      `-'-.-/   /.- (.''--'`-`-'  `--':        /     */
+/*                  -'            (   \  / .-._.).--..-._..  .-.  .-../ .-.   */
+/*   Created: 27-01-2022  by       `-' \/ (   )/    (   )  )/   )(   / (  |   */
+/*   Updated: 27-01-2022 11:14 by      /\  `-'/      `-'  '/   (  `-'-..`-'-' */
+/*                                 `._;  `._;                   `-            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+#include <stdlib.h>
+
+/*
+	Removes a char from a string
+*/
+char	*ft_rm_char(char *src, size_t index)
+{
+	char	*res;
+	int		new_len;
+
+	if (index > ft_strlen(src) - 1)
+		return (NULL);
+	new_len = ft_strlen(src) - 1;
+	res = malloc(sizeof(char) * (new_len + 1));
+	if (res)
+	{
+		ft_bzero(res, new_len + 1);
+		ft_strlcat(res, src, index + 1);
+		ft_strlcat(res, &(src[index + 1]), new_len + 1);
+	}
+	return (res);
+}
---
+++ srcs/libft/strings/ft_rm_substr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                       .-.                       .                          */
+/*                      / -'                      /                           */
+/*       .  .-. .-.   -/--).--..-.  .  .-. .-.   /-.  .-._.)  (               */
+/*        )/   )   )  /  /    (  |   )/   )   ) /   )(   )(    )         .    */
+/*   By: '/   /   (`.'  /      `-'-.-/   /.- (.''--'`-`-'  `--':        /     */
+/*                  -'            (   \  / .-._.).--..-._..  .-.  .-../ .-.   */
+/*   Created: 27-01-2022  by       `-' \/ (   )/    (   )  )/   )(   / (  |   */
+/*   Updated: 27-01-2022 11:15 by      /\  `-'/      `-'  '/   (  `-'-..`-'-' */
+/*                                 `._;  `._;                   `-            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+#include <stdlib.h>
+
+/*
+	Removes a substring between start and end from str
+	start at 'index' for 'size' bytes
+*/
+char	*ft_rm_substr(char *src, size_t index, size_t size)
+{
+	char	*res;
+	int		new_len;
+
+	if (index > ft_strlen(src) - 1 || index + size > ft_strlen(src))
+		return (NULL);
+	new_len = ft_strlen(src) - (size);
+	res = malloc(sizeof(char) * (new_len + 1));
+	if (res)
+	{
+		ft_bzero(res, new_len + 1);
+		ft_strlcat(res, src, index + 1);
+		ft_strlcat(res, &(src[index + size]), new_len + 1);
+	}
+	return (res);
+}
